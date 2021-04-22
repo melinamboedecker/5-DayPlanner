af2f7d160adcbb6972900d0ab43f961615b1e25a
Melina Boedecker <melinamboedecker@gmail.com>
2021-04-22 05:35:17 -0700

    initial commit, html file started, current date put into header

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+//Display Current Day of week and month and date
+var currentDay = moment().format("dddd, MMMM Do");
+$("#current-day").text(currentDay);
+
+//Display a table with each row representing an hour of the workday
+
+var am9 = (moment().format("dddd, MMMM Do"));
+$("#9am").text(am9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
