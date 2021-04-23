c5787f5e24414369c0e88119196f52008d60d305
Melina Boedecker <melinamboedecker@gmail.com>
2021-04-22 18:01:54 -0700

    added table with one row per hour of the workday and made background color of center row change colors depending on current time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,12 +2,65 @@
 var currentDay = moment().format("dddd, MMMM Do");
 $("#current-day").text(currentDay);
 
-//Display a table with each row representing an hour of the workday
+//Change background color of text fields depending on current time
+var currentTime = parseInt(moment().format("H"));
+console.log(currentTime);
 
-var am9 = (moment().format("dddd, MMMM Do"));
-$("#9am").text(am9);
 
 
+ var am9 = moment().format("H");
+ var tryIt = $('#9')
+ tryIt.text(am9);
+
+var rows = $(".col-6");
+rows.text("Hello");
+
+var rowsArray = Array.from(rows);
+console.log(rowsArray);
+
+console.log(rowsArray[5].id);
+
+for (var i = 0; 1 < rowsArray.length; i++) {
+    let tempid = parseInt(rowsArray[i].id);
+    console.log(tempid);
+    if (tempid < currentTime) {
+        $(rowsArray[i]).addClass('red');
+    } else
+    if (tempid === currentTime) {
+        $(rowsArray[i]).addClass('gray');
+    } else 
+    if (tempid > currentTime) {
+        $(rowsArray[i]).addClass('green');
+    } 
+    else ($rowsArray[i]).addClass('white');
+}
+
+//  Array.from(rows).forEach(row => {
+//      console.log(row.id);
+//     let
+//       rowIdString = row.id,
+//       rowHour;
+//     if (rowIdString) {
+//       rowHour = parseInt(rowIdString);
+//       console.log(rowHour);
+//     }
+//     if (rowHour) {
+//       // Compares row id to current hour and sets color accordingly
+//       if (currentTime === rowHour) {
+//         setColor(row, "red");
+//       } else if ((currentTime < rowHour) && (currentTime > rowHour - 6)) {
+//         setColor(row, "green");
+//       } else if ((currentTime > rowHour) && (currentTime < rowHour + 6)) {
+//         setColor(row, "lightgrey");
+//       } else {
+//         setColor(row, "white");
+//       }
+//     }
+//   });
+
+//   function setColor(element, color) {
+//     element.style.backgroundColor = color;
+//   }
 
 
 
